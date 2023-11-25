cbde51cacb2802bd79ae84cafd35fd177990155c
TomaszGrzesiuk <tomaszgrzesiuk@o2.pl>
2023-11-25 14:30:01 +0100

    Zadanie domowe dzien 4

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿// deklaracja zmiennej imie typ string
+string imie = "Ewa";
+// deklaracja zmiennej plec
+bool toKobieta = true;
+// deklaracja wieku
+int wiek = 18;
+
+
+if (toKobieta == true)
+{
+    Console.WriteLine("Dziewczyna");
+
+    if (imie == "Ewa")
+    {
+        Console.WriteLine("ma na imię Ewa");
+        if (wiek >= 18)
+        {
+            Console.WriteLine("Dorosły");
+        }
+        else
+        {
+            Console.WriteLine("Dziecko");
+        }
+    }
+    else
+    {
+        Console.WriteLine("nie ma na imię Ewa");
+        if (wiek >= 18)
+        {
+            Console.WriteLine("Dorosły");
+        }
+        else
+        {
+            Console.WriteLine("Dziecko");
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Chłopak");
+    if (wiek >= 18)
+    {
+        Console.WriteLine("Dorosły");
+    }
+    else
+    {
+        Console.WriteLine("Dziecko");
+    }
+}
